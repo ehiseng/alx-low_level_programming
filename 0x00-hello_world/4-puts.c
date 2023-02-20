97607d513fd945413caacb872645729533008afb
ehiseng <ehiseng@yahoo.com>
2023-02-20 15:27:23 -0800

    got_that

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main  - Entry point
+ * main - main function
  *
- * Return: Always 0 (Success)
+ * Return: always 0
  */
+
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
