0cd4bd058beb342b58d691fc03654d961f58f52c
ehiseng <ehiseng@yahoo.com>
2023-02-20 18:49:59 -0800

    for_skippin

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char p;
+
+	for (p = 'A' ; p <= 'Z' ; p++)
+		if (p != 'q' && p != 'e')
+			putchar(p);
+	putchar('\n');
+	return (0);
+}
