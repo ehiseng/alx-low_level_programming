ae6072044572901255469b3d35c50a038307ca97
ehiseng <ehiseng@yahoo.com>
2023-02-27 15:48:28 -0800

    swapping_two_int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - function that interchanges two int
+ * @a: first int
+ * @b: second int
+ */
+void swap_int(int *a, int *b)
+{
+	int swp
+		swp = *a;
+	*a = *b;
+	*b + swp;
+}
