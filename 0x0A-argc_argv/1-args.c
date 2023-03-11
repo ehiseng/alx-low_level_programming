966755422cbc9d21671020d159f18b1224f75c39
ehiseng <ehiseng@yahoo.com>
2023-03-10 16:34:05 -0800

    task_1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: count argument
+ * @argv: vector argument
+ * Return: Always 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
